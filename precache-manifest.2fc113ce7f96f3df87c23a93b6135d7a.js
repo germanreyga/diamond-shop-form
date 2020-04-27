7f3d56d1c429a8ef8f3291e49001516d1db7b1d8
German <german.reyga@gmail.com>
2020-04-26 20:10:13 -0500

    Updates

---
+++ precache-manifest.2fc113ce7f96f3df87c23a93b6135d7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e744337fbb0beb6294bf15442468c682",
+    "url": "/diamond-shop-form/index.html"
+  },
+  {
+    "revision": "5745a338e4fc99d77f85",
+    "url": "/diamond-shop-form/static/css/2.d42e62a9.chunk.css"
+  },
+  {
+    "revision": "621aa2832da1b1391200",
+    "url": "/diamond-shop-form/static/css/main.a0bf0f94.chunk.css"
+  },
+  {
+    "revision": "5745a338e4fc99d77f85",
+    "url": "/diamond-shop-form/static/js/2.d351e9b9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/diamond-shop-form/static/js/2.d351e9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621aa2832da1b1391200",
+    "url": "/diamond-shop-form/static/js/main.f92df082.chunk.js"
+  },
+  {
+    "revision": "05475cc3537b5faa2c32",
+    "url": "/diamond-shop-form/static/js/runtime-main.4c5244c7.js"
+  },
+  {
+    "revision": "fb98ed1700e8dfaf0764c11fc36a0a05",
+    "url": "/diamond-shop-form/static/media/NunitoSans-Regular.fb98ed17.ttf"
+  }
+]);
